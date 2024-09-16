67e363c0d3c8425bcac293ba9a44546383de3497
Ale Vitorino <vitorino.ale123@gmail.com>
2024-09-15 23:33:36 -0300

    Saque, Extrato e Depósito são realizados através de funções agora

--- desafio.py
+++ desafio.py
@@ -36,13 +36,50 @@ def extrato_saque(valor1):
     for i in range(len(valor1)):
       print(f'{i+1}º Saque: R$ {valor1[i]:.2f}')
 
+def depo(valor,depositos,saldo,/):#Posição 
+  if valor>0:
+    saldo += valor
+    depositos.append(valor)
+    print('Depósito realizado com sucesso.')
+  else:
+    print('Valor inválido.')
+
+  return saldo,depositos
+
+def saque(*,valor,numero_saques,limites_saques,saldo,limite,saques):#keyword only
+  if valor>0:
+    if numero_saques<limites_saques and valor<=saldo and valor<=limite:
+      saldo -= valor
+      numero_saques +=1
+      saques.append(valor)
+      print('Saque realizado com sucesso.')
+    elif numero_saques>=limites_saques:
+          print('Limite na quantidade de saques atingido, tente novamente amanhã.')
+    elif valor>limite:
+      print('Limite de R$ 500,00 por saque, tente novamente.')
+
+    elif valor>saldo:
+      print('Saldo insuficiênte para o valo desejado, tente novamente com outro valor.')
+    else:
+      print('Valor inválido.')
+
+  return saldo, numero_saques, saques
+  
+def ext_completo(saldo,/,*,saque,deposito):#Posição e Keyword
+  if len(saque) + len(deposito) == 0:
+    print('Não foram realizadas Movimentações.')
+  else:
+    print(f'Foram realizadas {len(saque) + len(deposito)} Operações: {len(saque)} Saque(s) e {len(deposito)} Deposito(s).')
+    extrato_deposito(deposito)
+    extrato_saque(saque)
+    verificar_saldo(saldo)
+
 saldo = 0
 limite = 500
 extrato = True
-conta = []
-depositos = []
-saques = []
-numero_saques = 0
+depositosT = []
+saquesT = []
+numero_de_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
@@ -51,33 +88,20 @@ while True:
 
   if opcao == "1": #Deposito
     valor = float(input('Informe o valor que deseja depositar: '))
-    if valor>0:
-      saldo += valor
-      depositos.append(valor)
-      print('Depósito realizado com sucesso.')
-    else:
-      print('Valor inválido.')
+
+    saldo, depositosT = depo(valor,depositosT,saldo)
 
   elif opcao == "2": #Saque
     valor = float(input('Informe o valor que deseja sacar: '))
 
-    if valor>0:
-      if numero_saques<LIMITE_SAQUES and valor<=saldo and valor<=limite:
-        saldo -= valor
-        numero_saques +=1
-        saques.append(valor)
-        print('Saque realizado com sucesso.')
-
-      elif numero_saques>=LIMITE_SAQUES:
-        print('Limite na quantidade de saques atingido, tente novamente amanhã.')
-
-      elif valor>limite:
-        print('Limite de R$ 500,00 por saque, tente novamente.')
-
-      elif valor>saldo:
-        print('Saldo insuficiênte para o valo desejado, tente novamente com outro valor.')
-    else:
-      print('Valor inválido.')
+    saldo, numero_de_saques, saquesT = saque(
+      valor=valor,
+      numero_saques=numero_de_saques,
+      limites_saques=LIMITE_SAQUES,
+      saldo=saldo,
+      limite=limite,
+      saques=saquesT
+    )
 
   elif opcao == "3": #Extrato
 
@@ -85,22 +109,16 @@ while True:
       entrada = input(menu_extrato)
 
       if entrada == 'D': #Somente Depositos
-        extrato_deposito(depositos)
+        extrato_deposito(depositosT)
 
       elif entrada == 'R': #Somente Saques
-        extrato_saque(saques)
+        extrato_saque(saquesT)
 
       elif entrada == 'S': #Somente Saldo
         verificar_saldo(saldo)
 
       elif entrada == 'C': #Extrato Completo
-        if len(saques) + len(depositos) == 0:
-          print('Não foram realizadas Movimentações.')
-        else:
-          print(f'Foram realizadas {len(saques) + len(depositos)} Operações: {len(saques)} Saque(s) e {len(depositos)} Deposito(s).')
-          extrato_deposito(depositos)
-          extrato_saque(saques)
-          verificar_saldo(saldo)   
+        ext_completo(saldo,saque=saquesT,deposito=depositosT)
 
       elif  entrada == "Q": #Sair de Extratos
           extrato = False
